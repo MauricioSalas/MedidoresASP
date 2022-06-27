e450a8e42f15cfecdf24d2e3943a45b97cff1ca8
MauricioSalas <mauricio.salas16@inacapmail.cl>
2022-06-26 21:00:46 -0400

    LecturaDALText (yyyy-MM-dd HH:mm)

--- MedidoresModel/DAL/LecturaDALText.cs
+++ MedidoresModel/DAL/LecturaDALText.cs
@@ -11,8 +11,8 @@ namespace MedidoresModel.DAL
     {
         private static List<Medidor> listaLecturas = new List<Medidor>()
         {
-            new Medidor() {Codigo = 3111, Tipo = "Inteligente", FechaUnix = "2022-05-24 23:14:15", KWh = 600},
-            new Medidor() {Codigo = 3112, Tipo = "Inteligente", FechaUnix = "2022-05-28 11:15:15", KWh = 500}
+            new Medidor() {Codigo = 3111, Tipo = "Inteligente", FechaUnix = "2022-05-24 23:14", KWh = 600},
+            new Medidor() {Codigo = 3112, Tipo = "Inteligente", FechaUnix = "2022-05-28 11:15", KWh = 500}
         };
         public void AgregarLectura(Medidor medidor)
         {
